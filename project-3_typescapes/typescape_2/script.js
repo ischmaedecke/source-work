51237aec145f300d402c3e17b87f3f6408cf7949
ischmaedecke <73975953+ischmaedecke@users.noreply.github.com>
2022-12-06 03:15:13 -0500

    yo

--- project-3_typescapes/typescape_2/script.js
+++ project-3_typescapes/typescape_2/script.js
@@ -1,92 +1,25 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let i = 0;
-
-function myLoop() {               //  create a loop function
-
-
-  setTimeout(function () {        //  call a 3s setTimeout when the loop is called
-
-    document.documentElement.style.setProperty(`--zindexController`, i);
-    i++;                          //  increment the counter
-
-    if (i < 102) {                //  if the counter < 101, call the loop function
-      setTimeout(myLoop, 0);
-      console.log('hi');
-    }
-  
-    if (i == 102) {
-    i = 0;
-    let restart;
-    clearTimeout(restart);
-    restart = setTimeout(myLoop, 100);
-    console.log('hello second if');
-    }
-  }, 100)
-}
-
-myLoop();                   //  start the loop
-
-
-
-
 // let i = 0;
 
-// function myLoop() {               //  create a loop function
+// function myLoop() {
 
-
-//   setTimeout(function () {        //  call a 3s setTimeout when the loop is called
+//   setTimeout(function () {
 
 //     document.documentElement.style.setProperty(`--zindexController`, i);
-//     i++;                          //  increment the counter
+//     i++;
 
-//     if (i < 102) {                //  if the counter < 101, call the loop function
-//       setTimeout(myLoop, 50);
+//     if (i < 102) {
+//       setTimeout(myLoop, 0);
 //       console.log('hi');
-//     }     
-//   }, 100)
+//     }
   
+//     if (i == 102) {
+//     i = 0;
+//     let restart;
+//     clearTimeout(restart);
+//     restart = setTimeout(myLoop, 100);
+//     console.log('hello second if');
+//     }
+//   }, 100)
 // }
 
-// let t=0
-// let times = 2
-// function repeat (myLoop, times){
-//   for (t=0; t<times; t++){
-//     myLoop();
-//   }
-// }
-
-// let restart;
-// clearTimeout(restart);
-// restart = setTimeout(myLoop, 100);
-// console.log('hello second if');
-
-
+// myLoop();
