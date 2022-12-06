d8b3a2b93515e56b552e98a831e5181d691850d3
ischmaedecke <73975953+ischmaedecke@users.noreply.github.com>
2022-12-06 14:06:43 -0500

    hello

--- project-3_typescapes/typescape_1-v2/script.js
+++ project-3_typescapes/typescape_1-v2/script.js
@@ -1,33 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let i = 0;
 
 function myLoop() {               //  create a loop function
@@ -54,39 +24,3 @@ function myLoop() {               //  create a loop function
 }
 
 myLoop();                   //  start the loop
-
-
-
-
-// let i = 0;
-
-// function myLoop() {               //  create a loop function
-
-
-//   setTimeout(function () {        //  call a 3s setTimeout when the loop is called
-
-//     document.documentElement.style.setProperty(`--zindexController`, i);
-//     i++;                          //  increment the counter
-
-//     if (i < 102) {                //  if the counter < 101, call the loop function
-//       setTimeout(myLoop, 50);
-//       console.log('hi');
-//     }     
-//   }, 100)
-  
-// }
-
-// let t=0
-// let times = 2
-// function repeat (myLoop, times){
-//   for (t=0; t<times; t++){
-//     myLoop();
-//   }
-// }
-
-// let restart;
-// clearTimeout(restart);
-// restart = setTimeout(myLoop, 100);
-// console.log('hello second if');
-
-
